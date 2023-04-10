38221324c74ac6d4c79b826f62f74b8714404d9a
Jack <jackboakes273@gmail.com>
2023-04-11 09:52:26 +1200

    add methods and annotations to Performer class

--- concert-service/src/main/java/proj/concert/service/domain/Performer.java
+++ concert-service/src/main/java/proj/concert/service/domain/Performer.java
@@ -1,11 +1,10 @@
 package proj.concert.service.domain;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 import proj.concert.common.types.Genre;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "PERFORMERS")
@@ -13,7 +12,7 @@ public class Performer {
 
     @Id
     @Column(name = "ID")
-    private Long Id;
+    private Long id;
 
     @Column(name = "NAME")
     private String name;
@@ -22,9 +21,67 @@ public class Performer {
     private String imageName;
 
     @Column(name = "GENRE")
+    @Enumerated(EnumType.STRING)
     private Genre genre;
 
     @Column(name = "BLURB")
     private String blurb;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageUri(String imageName) {
+        this.imageName = imageName;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public String getBlurb(){
+        return blurb;
+    }
+
+    public void setBlurb(String blurb){
+        this.blurb = blurb;
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if (!(obj instanceof Performer))
+            return false;
+        if (obj == this)
+            return true;
+
+        Performer rhs = (Performer) obj;
+        return new EqualsBuilder().
+                append(name, rhs.name).
+                isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 31).append(name).hashCode();
+    }
 }
