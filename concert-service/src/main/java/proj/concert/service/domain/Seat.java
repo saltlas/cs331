2f27cc51497d8b195820cb3e9a59b132fc553b19
NachoToast <32235595+NachoToast@users.noreply.github.com>
2023-04-11 17:31:56 +1200

    Add JPA annotations to `Seat` class

--- concert-service/src/main/java/proj/concert/service/domain/Seat.java
+++ concert-service/src/main/java/proj/concert/service/domain/Seat.java
@@ -2,15 +2,61 @@ package proj.concert.service.domain;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.util.Objects;
 
-public class Seat{
+@Entity
+@Table(name = "SEATS")
+public class Seat {
+	@Column(name = "LABEL")
+	private String label;
 
-    // TODO Implement this class.
+	@Column(name = "IS_BOOKED")
+	private boolean isBooked;
 
-	public Seat(String label, boolean isBooked, LocalDateTime date, BigDecimal cost) {	
-	}	
+	@ManyToOne
+	@JoinColumn(name = "CONCERT_DATE", nullable = false)
+	private ConcertDate concertDate;
+
+	@Column(name = "COST")
+	private BigDecimal cost;
+
+	public Seat(String label, boolean isBooked, ConcertDate date, BigDecimal cost) {	
+		this.label = label;
+		this.isBooked = isBooked;
+		this.concertDate = date;
+		this.cost = cost;
+	}
 	
-	public Seat() {}	
+	public Seat() {}
+	
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public boolean isBooked() {
+		return isBooked;
+	}
+
+	public void setBooked(boolean isBooked) {
+		this.isBooked = isBooked;
+	}
+
+	public ConcertDate getConcertDate() {
+		return concertDate;
+	}
+
+	public void setConcertDate(ConcertDate concertDate) {
+		this.concertDate = concertDate;
+	}
+
+	public BigDecimal getCost() {
+		return cost;
+	}
+
+	public void setCost(BigDecimal cost) {
+		this.cost = cost;
+	}
 }
