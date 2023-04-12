56226c6709d4f978d74c0efd752089b01b8d9900
NachoToast <32235595+NachoToast@users.noreply.github.com>
2023-04-12 14:16:52 +1200

    add ID getter and setter

--- concert-service/src/main/java/proj/concert/service/domain/Booking.java
+++ concert-service/src/main/java/proj/concert/service/domain/Booking.java
@@ -30,6 +30,14 @@ public class Booking {
     public Booking() {
     }
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
     public void setDate(ConcertDate date) {
         this.date = date;
     }
