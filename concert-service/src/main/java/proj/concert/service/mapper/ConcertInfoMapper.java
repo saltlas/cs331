17b67cb4903e661740e14bd540c7b015084cfdd5
NachoToast <32235595+NachoToast@users.noreply.github.com>
2023-04-24 20:41:56 +1200

    comment/lint: `ConcertInfoMapper.java` mapper

--- concert-service/src/main/java/proj/concert/service/mapper/ConcertInfoMapper.java
+++ concert-service/src/main/java/proj/concert/service/mapper/ConcertInfoMapper.java
@@ -7,21 +7,24 @@ import proj.concert.common.dto.ConcertInfoNotificationDTO;
 import java.util.ArrayList;
 import java.util.List;
 
-
-
+/**
+ * Mapper class for converting between {@link ConcertInfoNotificationDTO}s and
+ * {@link ConcertDate}s.
+ */
 public abstract class ConcertInfoMapper {
 
+	/** Converts a concert date into a info notification DTO. */
+	public static ConcertInfoNotificationDTO convert(ConcertDate concertDate) {
+		List<Seat> seats = new ArrayList<Seat>(concertDate.getSeats());
 
-	public static ConcertInfoNotificationDTO convert(ConcertDate concertDate){
-		List<Seat> seats = new ArrayList(concertDate.getSeats());
 		int unbooked = 0;
-		for(Seat seat: seats){
-			if(!(seat.isBooked())){
+
+		for (Seat seat: seats) {
+			if (!(seat.isBooked())) {
 				unbooked += 1;
 			}
 		}
-		ConcertInfoNotificationDTO dto = new ConcertInfoNotificationDTO(unbooked);
 
-		return dto;
+		return new ConcertInfoNotificationDTO(unbooked);
 	}
 }
